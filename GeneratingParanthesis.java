312e92fce4542c8ed0e13e21147b5698d7f58137
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-07-08 14:40:12 -0500

    Update GeneratingParanthesis.java

--- GeneratingParanthesis.java
+++ GeneratingParanthesis.java
@@ -4,38 +4,6 @@ import java.util.List;
 public class GeneratingParanthesis {
 
   private void recFn(int n, StringBuilder sb, int opbc, int clbc, List<String> res) {
-    if (clbc == n && clbc == opbc) {
-      res.add(sb.toString());
-      return;
-    }
-    if (opbc < n) {
-      recFn(n, sb.append("("), opbc + 1, clbc, res);
-      sb.deleteCharAt(sb.length() - 1);
-    }
-    if (clbc < opbc) {
-      recFn(n, sb.append(")"), opbc, clbc + 1, res);
-      sb.deleteCharAt(sb.length() - 1);
-    }
-  }
-
-  private List<String> generate(int n) {
-    List<String> res = new ArrayList<>();
-    recFn(n, new StringBuilder(), 0, 0, res);
-    return res;
-  }
-
-  public static void main(String[] args) {
-    List<String> res = new ArrayList<>();
-    System.out.println(new GeneratingParanthesis().generate(3));
-  }
-}
-
-// import java.util.ArrayList;
-// import java.util.List;
-
-// public class GeneratingParanthesis {
-
-  private void recFn(int n, StringBuilder sb, int opbc, int clbc, List<String> res) {
     System.out.println(sb + " " + opbc + " " + clbc);
     if (clbc == n && clbc == opbc) {
       res.add(sb.toString());
