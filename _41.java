b57a81464268445e5f4c84fe868fed4ddd3ffe99
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-07-20 19:09:59 -0500

    Update _41.java

--- _41.java
+++ _41.java
@@ -1,33 +1,41 @@
 import java.util.Arrays;
-
 class Solution {
 
+  private void remove_negatives_and_out_of_bound_elements(int[] nums) {
+    for (int i = 0; i < nums.length; i++)
+      if (nums[i] <= 0 || nums[i] > nums.length)
+        nums[i] = 1;
+  }
+
   public int firstMissingPositive(int[] nums) {
-    boolean seenOne = false;
-    int n = nums.length;
-    for (int i = 0; i < n; i++) {
-      if (nums[i] == 1) seenOne = true;
-      if (nums[i] <= 0 || nums[i] > n) nums[i] = 1;
+    // If 1 is not present we will return 1 as ans.
+    boolean _1_is_present = false;
+    for (int i = 0; i < nums.length; i++)
+      if (nums[i] == 1)
+        _1_is_present = true;
+    if (!_1_is_present)
+      return 1;
+
+    // We will remove all the -ve elements as well as the elements which are out-of-bound i.e., (greater than the len of array).
+    remove_negatives_and_out_of_bound_elements(nums);
+
+    // If 1 is present, then we need to define a clever way to find the first positive number.
+    // To come up with a clever solution, let us assume that the elements present in the array are our indexes. This assumption will help us to mark the elements as present which are in the array. Let us mark the presence as -ve i.e, if 'a', then mark it as '-a'.
+    for (int i = 0; i < nums.length; i++) {
+      int index = Math.abs(nums[i]) - 1;
+      if (nums[index] > 0)
+        nums[index] *= -1;
     }
 
-    // We have converted all the negatives into first positive i.e., 1
-    // We have also converted all the elements that are greater than the length of the array.
-    if (seenOne == false) return 1;
+    // At this point while iterating over the array, the first positive number will imply the first missing value
+    for (int i = 0; i < nums.length; i++)
+      if (nums[i] > 0)
+        return i + 1;
 
-    System.out.println(Arrays.toString(nums));
-
-    for (int i = 0; i < n; i++) {
-      int index = Math.abs(nums[i]); //  Every element of the array can be an index at this point of time because we have tranformed all the greater & negative numbers to 1.
-      int idx = index - 1;
-      if (nums[idx] > 0) nums[idx] *= -1; // An index will be marked as negative if the index is present in the array as value.
-    }
-
-    System.out.println(Arrays.toString(nums));
-    for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
-    return n + 1;
+    // Else just return the element that is nums.length + 1 as first positive missing value
+    return nums.length + 1;
   }
 }
-
 public class _41 {
 
   public static void main(String[] args) {
