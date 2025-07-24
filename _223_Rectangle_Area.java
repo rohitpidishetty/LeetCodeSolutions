a4ee5f7f218201c8d07fb3201168e7eafb9802d0
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-07-23 23:02:34 -0500

    Update _223_Rectangle_Area.java

--- _223_Rectangle_Area.java
+++ _223_Rectangle_Area.java
@@ -19,22 +19,23 @@ class Solution {
     int sx2 = bx2;
     int sy2 = by2;
 
-    int r1b = fx2 - fx1;
-    int r1h = fy2 - fy1;
-    int r2b = sx2 - sx1;
-    int r2h = sy2 - sy1;
+    int r1b = fx2 - fx1; // Rectangle 1 base
+    int r1h = fy2 - fy1; // Rectangle 1 height
+    int r2b = sx2 - sx1; // Rectangle 2 base
+    int r2h = sy2 - sy1; // Rectangle 2 height
 
+    // Finding over-lap
     int discX1 = Math.max(fx1, sx1);
     int discX2 = Math.min(fx2, sx2);
     int discY1 = Math.max(fy1, sy1);
     int discY2 = Math.min(fy2, sy2);
-    int discB = discX2 - discX1;
-    int discH = discY2 - discY1;
 
+    int discB = discX2 - discX1; // Overlapped rectangle base
+    int discH = discY2 - discY1; // Overlapped rectangle height
+
+    // If over-lapping then obviously it will have base and height, thus (> 0).
     int discard = 0;
-    if (discB > 0 && discH > 0) {
-      discard = discB * discH;
-    }
+    if (discB > 0 && discH > 0) discard = discB * discH;
 
     return (r1h * r1b) + (r2h * r2b) - discard;
   }
