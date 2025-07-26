7ccb473812e630240aa4c29c8acd3d06cf81d7ae
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-07-26 11:59:32 -0500

    Add files via upload

---
+++ _237_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,15 @@
+
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+ 
+class Solution {
+  public void deleteNode(ListNode node) {
+
+  }
+}
+public class _237_Delete_Node_in_a_Linked_List {
+  
+}
