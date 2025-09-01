0c4c8c17b6563bccbf3abc11e34a3dd6bd0b08f9
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-09-01 15:04:44 -0500

    Add files via upload

---
+++ _319_Bulb_Switcher.java
@@ -0,0 +1,13 @@
+class Solution {
+
+  public int bulbSwitch(int n) {
+    return 0;
+  }
+}
+
+public class _319_Bulb_Switcher {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().bulbSwitch(3));
+  }
+}
