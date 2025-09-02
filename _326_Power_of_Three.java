2d971fa0461f06787f4c70e6140ab8a6fb7af146
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-09-02 16:05:13 -0500

    Add files via upload

--- _326_Power_of_Three.java
+++ _326_Power_of_Three.java
@@ -1,14 +1,9 @@
 class Solution {
 
   public boolean isPowerOfThree(int n) {
-    if (n < 0) return false;
-    int first_bit = ((n & (1 << 0)) == 0) ? 0 : 1;
-    int second_bit = ((n & (1 << 1)) == 0) ? 0 : 1;
-    int third_bit = ((n & (1 << 2)) == 0) ? 0 : 1;
-    if (
-      first_bit == 1 && (second_bit == 0 || second_bit == 1) && third_bit == 0
-    ) return true;
-    return false;
+    if (n <= 0) return false;
+    while (n % 3 == 0) n /= 3;
+    return n == 1;
   }
 }
 
