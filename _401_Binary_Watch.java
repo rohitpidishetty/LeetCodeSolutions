3537faa9115baebfa00f3319d5d2c8493ddf7fbb
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-09-20 20:15:50 -0500

    Add files via upload

---
+++ _401_Binary_Watch.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+class Solution {
+
+  public List<String> readBinaryWatch(int turnedOn) {
+    return null;
+  }
+}
+
+public class _401_Binary_Watch {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().readBinaryWatch(1));
+  }
+}
