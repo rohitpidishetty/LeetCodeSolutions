0a4ee904c91c46acb9750979c8c4e5183ec3ec18
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-11-06 17:12:40 -0600

    Add files via upload

---
+++ _600_Non_negative_Integers_without_Consecutive_Ones.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int findIntegers(int n) {
+    return 0;
+  }
+}
+
+public class _600_Non_negative_Integers_without_Consecutive_Ones {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().findIntegers(5));
+  }
+}
