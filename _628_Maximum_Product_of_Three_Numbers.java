af9828bbcba17e6b66e0424b48dee84cb996f7e4
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-11-11 10:44:53 -0600

    Add files via upload

---
+++ _628_Maximum_Product_of_Three_Numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+
+  public int maximumProduct(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length - 1;
+    return Math.max(
+      (nums[n] * nums[n - 1] * nums[n - 2]),
+      (nums[0] * nums[1] * nums[n])
+    );
+  }
+}
+
+public class _628_Maximum_Product_of_Three_Numbers {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().maximumProduct(new int[] { 1, 2, 3 }));
+  }
+}
