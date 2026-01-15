1936132582ecb4dc8f3ad1021df2ec6e35a92e34
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-01-14 21:06:46 -0600

    Add files via upload

---
+++ _796_Rotate_String.java
@@ -0,0 +1,13 @@
+class Solution {
+
+  public boolean rotateString(String s, String goal) {
+    return false;
+  }
+}
+
+public class _796_Rotate_String {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().rotateString("abcde", "abcde"));
+  }
+}
