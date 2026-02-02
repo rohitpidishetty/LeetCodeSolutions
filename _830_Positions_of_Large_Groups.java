0dcc5a2d714bf1184d389b4093a36e40ec5c3b5a
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-02-02 17:55:40 -0600

    Add files via upload

---
+++ _830_Positions_of_Large_Groups.java
@@ -0,0 +1,13 @@
+class Solution {
+
+  public List<List<Integer>> largeGroupPositions(String s) {
+    return null;
+  }
+}
+
+public class _830_Positions_of_Large_Groups {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().largeGroupPositions("abbxxxxzzy"));
+  }
+}
