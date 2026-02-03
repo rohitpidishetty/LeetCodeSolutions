63d1002d11d5e048bc0c2ed204125ffacb740fe3
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-02-03 12:23:01 -0600

    Add files via upload

--- _830_Positions_of_Large_Groups.java
+++ _830_Positions_of_Large_Groups.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
 
   public List<List<Integer>> largeGroupPositions(String s) {
-    return null;
+    int j = 0,
+      i = 0,
+      n = s.length();
+    List<List<Integer>> buffer = new ArrayList<>();
+    while (j < n) {
+      char ch = s.charAt(j);
+      if (!(j + 1 < n && ch == s.charAt(j + 1))) {
+        if (j - i + 1 >= 3) buffer.add(Arrays.asList(i, j));
+        i = j + 1;
+      }
+      j++;
+    }
+
+    return buffer;
   }
 }
 
 public class _830_Positions_of_Large_Groups {
 
   public static void main(String[] args) {
-    System.out.println(new Solution().largeGroupPositions("abbxxxxzzy"));
+    System.out.println(new Solution().largeGroupPositions("abcdddeeeeaabbbcd"));
   }
 }
