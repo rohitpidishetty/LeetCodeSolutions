f7fca1c5d56ec503ca3df38354f792c71ba53dfc
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-02-04 20:28:46 -0600

    Add files via upload

---
+++ _836_Rectangle_Overlap.java
@@ -0,0 +1,18 @@
+class Solution {
+
+  public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+    return false;
+  }
+}
+
+public class _836_Rectangle_Overlap {
+
+  public static void main(String[] args) {
+    System.out.println(
+      new Solution().isRectangleOverlap(
+        new int[] { 0, 0, 2, 2 },
+        new int[] { 1, 1, 3, 3 }
+      )
+    );
+  }
+}
