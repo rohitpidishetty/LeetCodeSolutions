6fa73e55681de8121b15ec967781e06235c51a18
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-02-15 01:53:33 -0600

    Add files via upload

--- _851_Loud_and_Rich.java
+++ _851_Loud_and_Rich.java
@@ -1,9 +1,55 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
 
+  private int[] ans;
+
+  private Map<Integer, List<Integer>> buildGraph(
+    int[][] arr,
+    int n,
+    int[] quietness
+  ) {
+    Map<Integer, List<Integer>> connections = new HashMap<>();
+    for (int i = 0; i < n; i++) connections.put(i, new ArrayList<>());
+    for (int[] ar : arr) connections.get(ar[1]).add(ar[0]);
+    // DAG
+    return connections;
+  }
+
+  private int dfs(int source, Map<Integer, List<Integer>> graph, int[] quiet) {
+    if (this.ans[source] != Integer.MAX_VALUE) return this.ans[source];
+    List<Integer> children = graph.get(source);
+    if (children.size() == 0) {
+      this.ans[source] = source;
+      return source;
+    }
+
+    // Get children of current source node
+    int best = source;
+    for (Integer nm : children) {
+      int current = dfs(nm, graph, quiet);
+      if (quiet[current] < quiet[best]) best = current;
+    }
+
+    this.ans[source] = best;
+
+    return best;
+  }
+
   public int[] loudAndRich(int[][] richer, int[] quiet) {
-    return null;
+    int n = quiet.length;
+    Map<Integer, List<Integer>> graph = buildGraph(richer, n, quiet);
+    this.ans = new int[n];
+    Arrays.fill(this.ans, Integer.MAX_VALUE);
+    for (int i = 0; i < n; i++) {
+      dfs(i, graph, quiet);
+    }
+
+    return this.ans;
   }
 }
 
