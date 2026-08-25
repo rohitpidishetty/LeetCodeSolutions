e8cf5f451aac6826fe77ba7e626e750383bb1942
Er. P. Rohit V. Acharya <rohitpidishetty@gmail.com>
2026-08-24 20:19:07 -0500

    P3

--- StriversSheet/_26_Remove_Duplicates_from_Sorted_Array.java
+++ StriversSheet/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
 
   public int removeDuplicates(int[] nums) {
-    return 0;
+    int lazy = 0;
+    int curr = -101;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != curr) {
+        curr = nums[i];
+        nums[lazy] = curr;
+        lazy++;
+      }
+    }
+
+    return lazy;
   }
 }
 
 public class _26_Remove_Duplicates_from_Sorted_Array {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    System.out.println(
+      new Solution().removeDuplicates(
+        new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }
+      )
+    );
+  }
 }
