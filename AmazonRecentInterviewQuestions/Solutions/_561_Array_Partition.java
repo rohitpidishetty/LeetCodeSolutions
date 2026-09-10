5d231e9e5cd74540b313556afaca0978267a69f2
Er. P. Rohit V. Acharya <rohitpidishetty@gmail.com>
2026-09-09 20:19:18 -0500

    lc 561

---
+++ AmazonRecentInterviewQuestions/Solutions/_561_Array_Partition.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int sum = 0;
+    for (int i = 0; i < nums.length; i += 2) sum += nums[i];
+    return sum;
+  }
+}
+
+public class _561_Array_Partition {
+
+  public static void main(String[] args) {
+    System.out.println(
+      new Solution().arrayPairSum(new int[] { 6, 2, 6, 5, 1, 2 })
+    );
+  }
+}
